37ed375fca939db00fdd89434469fa3b6f37990e
Marielle <marielle@theredmoon.ca>
2021-04-24 21:54:41 -0400

    added footer component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import { Fragment } from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Home () {
     return(
         <Fragment>
             <HeroSection />
             <Cards />
+            <Footer />
         </Fragment>
     )
 }
